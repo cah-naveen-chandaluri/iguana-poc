8f973e2852f24bd13e3c73b653340c79d51a4dd1
admin <admin>
2019-11-01 08:30:14 +0000

    Export: exporting for trial

--- IGUANA_CHANNEL_VERSION-6_1_0/3PL_WO_Orders_to_Database-From-LANz52GeOCXLh9/main.lua
+++ IGUANA_CHANNEL_VERSION-6_1_0/3PL_WO_Orders_to_Database-From-LANz52GeOCXLh9/main.lua
@@ -4,22 +4,24 @@ function main()
    dbConnection = require("DBConnection")
    
    -- Read the XML file from the Directory
-   local url = "C:\\3PL_WO\\OrderFiles\\XMLdata.xml"
+   local url =iguana.project.root()..'other/XMLdata.xml'
+   --local url="C:\\3PL\\XMLdata.xml"
+   c="NO"
    
    if(GetFileExtension(url) == '.xml') then
    
      -- Open order file
-     local open_order_file = io.open(url, "r")
+     local open_order_file = io.open(iguana.project.root()..'other/XMLdata.xml', "r")
      -- Read order file
      local read_order_file =  open_order_file:read('*a')
      -- Close the file
      open_order_file:close()
      
      local order_data = xml.parse(read_order_file)  
-  
-     print(order_data) 
-  
-     dbConnection.connectdb()
+print(order_data)
+  print(os.time(),os.ts.time(),os.date(),os.date('%X'))
+    
+      dbConnection.connectdb()
       
      -- Complete two SQL insert statements for csos_order_header and csos_order_details below.
      -- Task 1 : Read the values from the 'order_data' which has data from the xml and **validate the each value based on the column type
@@ -40,34 +42,68 @@ function main()
                          [[
                            INSERT INTO csos_order_header
                            (
-                              
+                             CSOS_ORD_HDR_NUM,
+                             BUSINESS_UNIT,NO_OF_LINES,ORDER_CHANNEL,PO_DATE,PO_NUMBER,
+                             SHIPTO_NUM,UNIQUE_TRANS_NUM,
+                             ACTIVE_FLG,ROW_ADD_STP,ROW_ADD_USER_ID,ROW_UPDATE_STP,ROW_UPDATE_USER_ID
                            )
    VALUES
    (
    ]]..
       
-   "'"..Msg.breakfast_menu:child("food", i)["name"][1].."',"..
-   "\n   '"..Msg.breakfast_menu:child("food", i)["price"][1].."',"..
-   "\n   '"..Msg.breakfast_menu:child("food", i)["description"][1].."',"..
-   "\n   '"..Msg.breakfast_menu:child("food", i)["calories"][1].."'".. 
-   '\n   )'
+       "'"..CSOS_ORD_HDR_NUM.."',"..
+       "\n   '"..order_data.root.CSOSOrderRequest.CSOSOrder.OrderSummary.BusinessUnit:nodeText().."',"..
+       "\n   '"..order_data.root.CSOSOrderRequest.CSOSOrder.OrderSummary.NoOfLines:nodeText().."',"..
+       "\n   '"..order_data.root.CSOSOrderRequest.CSOSOrder.OrderSummary.OrderChannel:nodeText().."',".. 
+      "\n   '"..order_data.root.CSOSOrderRequest.CSOSOrder.OrderSummary.PODate:nodeText().."',"..
+       "\n   '"..order_data.root.CSOSOrderRequest.CSOSOrder.OrderSummary.PONumber:nodeText().."',"..
+       "\n   '"..order_data.root.CSOSOrderRequest.CSOSOrder.OrderSummary.ShipToNumber:nodeText().."',"..
+       "\n   '"..order_data.root.CSOSOrderRequest.CSOSOrder.OrderSummary.UniqueTransactionNumber:nodeText().."',"..
+       "\n   '"..c.."',"..
+       "\n   '"..os.date('%X').."',"..
+       "\n   '"..os.date('%X').."',"..
+       "\n   '"..os.date('%X').."',"..
+       "\n   '"..os.date('%X').."'".. 
+       '\n   )'
    
+      
+                      
+      
+      
+      
+      
 local sql_csos_order_details =
       
       
                          [[
                            INSERT INTO csos_order_details
                            (
-                              
+                            CSOS_ORD_DTL_NUM,CSOS_ORD_HDR_NUM,
+                            BUYER_ITEM_NUM,FORM,LINE_NUM,NAME_OF_ITEM,NATIONAL_DRUG_CDE,
+                            QUANTITY,DEA_SCHEDULE,SIZE_OF_PACKAGE,STRENGTH,SUPPLIER_ITEM_NUM,
+                            ACTIVE_FLG,ROW_ADD_STP,ROW_ADD_USER_ID,ROW_UPDATE_STP,ROW_UPDATE_USER_ID
                            )
    VALUES
    (
    ]]..
-   "'"..Msg.breakfast_menu:child("food", i)["name"][1].."',"..
-   "\n   '"..Msg.breakfast_menu:child("food", i)["price"][1].."',"..
-   "\n   '"..Msg.breakfast_menu:child("food", i)["description"][1].."',"..
-   "\n   '"..Msg.breakfast_menu:child("food", i)["calories"][1].."'".. 
-   '\n   )'
+      "'"..CSOS_ORD_DTL_NUM.."',"..
+      "\n   '"..CSOS_ORD_HDR_NUM.."',".. 
+      "\n   '"..order_data.root.CSOSOrderRequest.CSOSOrder.Order.OrderItem.BuyerItemNumber:nodeText().."',"..
+      "\n   '"..order_data.root.CSOSOrderRequest.CSOSOrder.Order.OrderItem.Form:nodeText().."',"..
+      "\n   '"..order_data.root.CSOSOrderRequest.CSOSOrder.Order.OrderItem.LineNumber:nodeText().."',"..
+      "\n   '"..order_data.root.CSOSOrderRequest.CSOSOrder.Order.OrderItem.NameOfItem:nodeText().."',"..
+      "\n   '"..order_data.root.CSOSOrderRequest.CSOSOrder.Order.OrderItem.NationalDrugCode:nodeText().."',"..
+      "\n   '"..order_data.root.CSOSOrderRequest.CSOSOrder.Order.OrderItem.QuantityOrdered:nodeText().."',"..
+      "\n   '"..order_data.root.CSOSOrderRequest.CSOSOrder.Order.OrderItem.Schedule:nodeText().."',"..
+      "\n   '"..order_data.root.CSOSOrderRequest.CSOSOrder.Order.OrderItem.SizeOfPackages:nodeText().."',"..
+      "\n   '"..order_data.root.CSOSOrderRequest.CSOSOrder.Order.OrderItem.Strength:nodeText().."',"..
+      "\n   '"..order_data.root.CSOSOrderRequest.CSOSOrder.Order.OrderItem.SupplierItemNumber:nodeText().."',"..
+      "\n   '"..c.."',"..
+      "\n   '"..os.date('%X').."',"..
+      "\n   '"..os.date('%X').."',"..
+      "\n   '"..os.date('%X').."',"..  
+      "\n   '"..os.date('%X').."'".. 
+      '\n   )'
    
 
     -- Execute the sql statements   
