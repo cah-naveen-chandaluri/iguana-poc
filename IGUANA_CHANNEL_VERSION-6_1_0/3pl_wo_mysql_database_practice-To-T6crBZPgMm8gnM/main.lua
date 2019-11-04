55e7b134cffe61d10d842aa180dd0b73405ed2ae
admin <admin>
2019-11-04 16:05:49 +0000

    Export: good

--- IGUANA_CHANNEL_VERSION-6_1_0/3pl_wo_mysql_database_practice-To-T6crBZPgMm8gnM/main.lua
+++ IGUANA_CHANNEL_VERSION-6_1_0/3pl_wo_mysql_database_practice-To-T6crBZPgMm8gnM/main.lua
@@ -3,15 +3,22 @@
 function main()
    dbConnection = require("DBConnection")
    
+   
+     i, t = 0, {}
+ pfile =io.popen([[dir "]].."C:\\3PL_WO\\OrderFiles\\"..[[" /b]])
+    for filename in pfile:lines() do
    -- Read the XML file from the Directory
-   local other_folder =iguana.project.root()..'other/XMLdata.xml'
-   --local url="C:\\3PL\\XMLdata.xml"
+   --local other_folder =iguana.project.root()..'other/XMLdata.xml'
+  local other_folder="C:\\3PL_WO\\OrderFiles\\"..filename
+   print(other_folder)
+      
  ACTIVE_FLG="NO"
    
    if(GetFileExtension(other_folder) == '.xml') then
-   
+   print(other_folder)
      -- Open order file
      local open_order_file = io.open(other_folder, "r")
+         print(open_order_file,other_folder)
      -- Read order file
      local read_order_file =  open_order_file:read('*a')
      -- Close the file
@@ -19,6 +26,8 @@ function main()
      
      local order_data = xml.parse(read_order_file)  
   print(order_data)
+   
+
     
       dbConnection.connectdb()
       
@@ -34,14 +43,19 @@ function main()
                   --ROW_UPDATE_STP      CURRENT_TIMESTAMP
                   --ROW_UPDATE_USER_ID	 SYSTEM
        
-      
+      --- **Validate all the data which we are storing into the two tables.
+         
+        -- 1. change the table name and column
+        -- 3. get the value from table 1 and store in table2
+         
+         
+         
        local sql_csos_order_header =
       
       
                          [[
                            INSERT INTO csos_order_header
                            (
-                             CSOS_ORD_HDR_NUM,
                              BUSINESS_UNIT,NO_OF_LINES,ORDER_CHANNEL,PO_DATE,PO_NUMBER,
                              SHIPTO_NUM,UNIQUE_TRANS_NUM,
                              ACTIVE_FLG,ROW_ADD_STP,ROW_ADD_USER_ID,ROW_UPDATE_STP,ROW_UPDATE_USER_ID
@@ -50,8 +64,8 @@ function main()
    (
    ]]..
       
-       "'"..CSOS_ORD_HDR_NUM.."',"..
-       "\n   '"..order_data.root.CSOSOrderRequest.CSOSOrder.OrderSummary.BusinessUnit:nodeText().."',"..
+       --"'"..CSOS_ORD_HDR_NUM.."',"..
+       "'"..order_data.root.CSOSOrderRequest.CSOSOrder.OrderSummary.BusinessUnit:nodeText().."',"..
        "\n   '"..order_data.root.CSOSOrderRequest.CSOSOrder.OrderSummary.NoOfLines:nodeText().."',"..
        "\n   '"..order_data.root.CSOSOrderRequest.CSOSOrder.OrderSummary.OrderChannel:nodeText().."',".. 
       "\n   '"..order_data.root.CSOSOrderRequest.CSOSOrder.OrderSummary.PODate:nodeText().."',"..
@@ -62,13 +76,13 @@ function main()
        "\n   '"..os.date().."',"..
        "\n   '"..ROW_ADD_USER_ID.."',"..
        "\n   '"..os.date().."',"..
-       "\n   '"..os.date(ROW_UPDATE_USER_ID).."'".. 
+       "\n   '"..ROW_UPDATE_USER_ID.."'".. 
        '\n   )'
    
        --conn:execute{sql=sql_csos_order_header, live=true}
      cursor=conn:query{sql=sql_csos_order_header,live=true}
-                    CSOS_ORD_HDR_NUM_UPDATE=cursor[CSOS_ORD_HDR_NUM] 
-      
+                    CSOS_ORD_HDR_NUM_UPDATE=cursor[CSOS_ORD_HDR_NUM]
+     -- if(cursor != null)
       
       
       
@@ -118,9 +132,10 @@ local sql_csos_order_details =
    end
    
 end
-
+end
 
 -- Validating the file extenstion format
 function GetFileExtension(other_folder)
      return other_folder:match("^.+(%..+)$")
 end
+   
