3aa4c281c77f7f5ad048a301cfcb974983c63684
admin <admin>
2019-11-25 11:28:02 +0000

    Export: commit

--- IGUANA_CHANNEL_VERSION-6_1_0/3PL_WO_Orders_to_db_channel_2-FromHTTP-3wqozcqsEL5Vh9/main.lua
+++ IGUANA_CHANNEL_VERSION-6_1_0/3PL_WO_Orders_to_db_channel_2-FromHTTP-3wqozcqsEL5Vh9/main.lua
@@ -1,4 +1,4 @@
---URL='https://spselitestg.cardinalhealth.net/stage_841/ws/DmsWebService'
+URL='https://spselitestg.cardinalhealth.net/stage_841/ws/DmsWebService'
 net.http.respond{headers='',body='HELLO',persist=false,code=5}
 function main()
 
@@ -47,36 +47,40 @@ function main()
 
        
 
-csos_order_header_data=conn_dev:query{sql="select UNIQUE_TRANS_NUM,PO_NUMBER,PO_DATE,SHIPTO_NUM from 3pl_sps_ordering.csos_order_header where CSOS_ORDER_HDR_STAT='1';", live=true};
-      print(csos_order_header_data[1].UNIQUE_TRANS_NUM,csos_order_header_data[1].PO_NUMBER,csos_order_header_data[1].PO_DATE,csos_order_header_data[1].SHIPTO_NUM)
+csos_order_header_data=conn_dev:query{sql="select CSOS_ORD_HDR_NUM,UNIQUE_TRANS_NUM,PO_NUMBER,PO_DATE,SHIPTO_NUM from 3pl_sps_ordering.csos_order_header where CSOS_ORDER_HDR_STAT='1';", live=true};
+
       
       
       for i=1,#csos_order_header_data do
-     order_header_data=conn_dev:query{sql="select CUSTOMER_NUM,CSOS_ORDER_NUM,PO_NUM,PO_DTE from 3pl_sps_ordering.order_header where CSOS_ORDER_NUM='"..csos_order_header_data[i].UNIQUE_TRANS_NUM.."';",live=true};
-print(order_header_data[1].CUSTOMER_NUM,order_header_data[1].PO_NUM,order_header_data[1].PO_DTE,order_header_data[1].CSOS_ORDER_NUM)
-        customer_billto_shipto_data=conn_dev:query{sql="select CUSTOMER_NUM,SHIPTO_NUM FROM 3pl_sps_ordering.customer_billto_shipto WHERE CUSTOMER_NUM='"..order_header_data[i].CUSTOMER_NUM.."';",live=true};
-         print(customer_billto_shipto_data[1].CUSTOMER_NUM,customer_billto_shipto_data[1].SHIPTO_NUM)
-print(csos_order_header_data[2].UNIQUE_TRANS_NUM)
+     order_header_data=conn_dev:query{sql="select ORDER_NUM,CUSTOMER_NUM,CSOS_ORDER_NUM,PO_NUM,PO_DTE from 3pl_sps_ordering.order_header where CSOS_ORDER_NUM='"..csos_order_header_data[i].UNIQUE_TRANS_NUM.."';",live=true};
 
---cursor5=conn_dev:query{sql="select * from 3pl_sps_ordering.order_header where CSOS_ORDER_NUM='"..csos_order_header_data[i].UNIQUE_TRANS_NUM.."' and PO_NUM='"..csos_order_header_data[i].PO_NUMBER.."' and PO_DTE='"..csos_order_header_data[i].PO_DATE.."';", live=true};
-  -- cursor6=conn_dev:query{sql="select * from csos_order_header where SHIPTO_NUM='"..customer_billto_shipto_data[i].SHIPTO_NUM.."';",live=true};
+        customer_billto_shipto_data=conn_dev:query{sql="select CUSTOMER_NUM,SHIPTO_NUM FROM 3pl_sps_ordering.customer_billto_shipto WHERE CUSTOMER_NUM='"..order_header_data[i].CUSTOMER_NUM.."';",live=true};
+        end
+      for i=1,#csos_order_header_data do
+
+cursor5=conn_dev:query{sql="select * from 3pl_sps_ordering.order_header where CSOS_ORDER_NUM='"..csos_order_header_data[i].UNIQUE_TRANS_NUM.."' and PO_NUM='"..csos_order_header_data[i].PO_NUMBER.."' and PO_DTE='"..csos_order_header_data[i].PO_DATE.."';", live=true};
+  cursor6=conn_dev:query{sql="select * from csos_order_header where SHIPTO_NUM='"..customer_billto_shipto_data[i].SHIPTO_NUM.."';",live=true};
         
-         end
-      if(csos_order_header_data[1].PO_NUMBER==order_header_data[1].PO_NUM and csos_order_header_data[1].PO_DATE==order_header_data[1].PO_DTE and 
-         csos_order_header_data[1].UNIQUE_TRANS_NUM==order_header_data[1].CSOS_ORDER_NUM and csos_order_header_data[1].SHIPTO_NUM==customer_billto_shipto_data[1].SHIPTO_NUM) then
+end    
+   if(cursor5~=nil and cursor6~=nil) then
+     -- if(csos_order_header_data[1].PO_NUMBER==order_header_data[1].PO_NUM and csos_order_header_data[1].PO_DATE==order_header_data[1].PO_DTE and 
+       --  csos_order_header_data[1].UNIQUE_TRANS_NUM==order_header_data[1].CSOS_ORDER_NUM and csos_order_header_data[1].SHIPTO_NUM==customer_billto_shipto_data[1].SHIPTO_NUM) then
          
-         print("hello")
-         
-       -- cursor7== it will check comparision for csos_order_details and order_details
-         
-         if(cursor7~=nil) then
+         for i=1,#csos_order_header_data do
+         order_details_data=conn_dev:query{sql="select REQ_QTY,SHIP_UOM_DESC,PROD_NUM from 3pl_sps_ordering.order_detail where ORDER_HDR_NUM='"..order_header_data[i].ORDER_NUM.."';",live=true};
+
+         prod_data=conn_dev:query{sql="select SKU_ITEM_ID,NDC_ID,DEA_SCHEDULE FROM 3pl_sps_ordering.prod where PROD_NAM='"..order_details_data[i].PROD_NUM.."';",live=true};
+       
+         csos_order_details_data=conn_dev:query{sql="select QUANTITY,BUYER_ITEM_NUM,NATIONAL_DRUG_CDE,DEA_SCHEDULE,SIZE_OF_PACKAGE FROM 3pl_sps_ordering.csos_order_details where CSOS_ORD_HDR_NUM='"..csos_order_header_data[i].CSOS_ORD_HDR_NUM.."';",live=true};
+        end
+            for i=1,#csos_order_header_data do
+               cursor7=conn_dev:query{sql="select * from  3pl_sps_ordering.order_detail where REQ_QTY='"..csos_order_details_data[i].QUANTITY.."' and SHIP_UOM_DESC='"..csos_order_details_data[i].SIZE_OF_PACKAGE.."';",live=true};
+         cursor8=conn_dev:query{sql="select * from 3pl_sps_ordering.prod where SKU_ITEM_ID='"..csos_order_details_data[i].BUYER_ITEM_NUM.."' and NDC_ID='"..csos_order_details_data[i].NATIONAL_DRUG_CDE.."' and DEA_SCHEDULE='"..csos_order_details_data[i].DEA_SCHEDULE.."';",live=true};
+            if(cursor7~=nil and cursor8~=nil) then
             
             
-          
-         
-         
-            
-         --[[  newFile = io.open( "C:\\3PL_WO\\SOAP\\soapdata.txt", "w+" )
+
+          newFile = io.open( "C:\\3PL_WO\\SOAP\\soapdata.txt", "w+" )
     --local R =net.http.post{url=URL,live=true}
    F=io.open("C:\\3PL_WO\\SOAP\\soaprequest.txt","r")
           Data =  F:read('*a')
@@ -131,17 +135,16 @@ print(csos_order_header_data[2].UNIQUE_TRANS_NUM)
     fp = io.open("C:\\3PL_WO\\SOAP\\soapdatacopy.txt", "r+" )
      Content =  fp:read('*a')
     print(Content)
-   
-   
-   ]]--
-            
+ 
             else
                 log_file:write("Data Present in csos_order_details and order_details tables are not equal "..os.date('%x').." at :"..os.date('%X'),"\n")
             end
+               end
          
          else 
          log_file:write("Data Present in csos_order_header and order_header tables are not equal "..os.date('%x').." at :"..os.date('%X'),"\n")
          end
+    
    else
         log_file:write("Database connection  is not exist on : "..os.date('%x').." at :"..os.date('%X'),"\n")
     end
