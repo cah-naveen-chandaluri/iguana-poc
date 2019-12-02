49c80201c5fb17d27b58e04bca1a9f1195c58ca6
admin <admin>
2019-12-02 18:13:54 +0000

    Export: commit

--- IGUANA_CHANNEL_VERSION-6_1_0/3PL_WO_Orders_to_db_channel_2-FromHTTP-3wqozcqsEL5Vh9/main.lua
+++ IGUANA_CHANNEL_VERSION-6_1_0/3PL_WO_Orders_to_db_channel_2-FromHTTP-3wqozcqsEL5Vh9/main.lua
@@ -18,7 +18,7 @@ function main()
     log_file = getLogFile(output_log_path)
     log_file:write(TIME_STAMP.."******* Iguana channel Started Running *******","\n")
 
-    count=1
+
     if pcall(DBConn) then
         selection_status = false
         conn_dev:execute{sql=[[START TRANSACTION;]] ,live=true};
@@ -44,39 +44,32 @@ function main()
                         order_details_data=conn_dev:query{sql="select REQ_QTY,SHIP_UOM_DESC,PROD_NUM from 3pl_sps_ordering.order_detail where ORDER_HDR_NUM='"..tostring(order_header_data[i].ORDER_NUM).."';",live=true};
                         if(#order_details_data>0) then
                             selection_status=true
-                     
-                     prod_data={}
+                            prod_data={}
                             for j=1,#order_details_data do
                                 prod_data[j]=conn_dev:query{sql="select SKU_ITEM_ID,NDC_ID,DEA_SCHEDULE FROM 3pl_sps_ordering.prod where PROD_NUM='"..tostring(order_details_data[j].PROD_NUM).."';",live=true};
                             end
-                  
-                     print(order_details_data[1].PROD_NUM,order_details_data[2].PROD_NUM)
+                            print(order_details_data[1].PROD_NUM,order_details_data[2].PROD_NUM)
                             csos_order_details_data=conn_dev:query{sql="select CSOS_ORD_HDR_NUM,QUANTITY,BUYER_ITEM_NUM,NATIONAL_DRUG_CDE,DEA_SCHEDULE,SIZE_OF_PACKAGE FROM 3pl_sps_ordering.csos_order_details where CSOS_ORD_HDR_NUM='"..tostring(csos_order_header_data[i].CSOS_ORD_HDR_NUM).."';",live=true};
-                            
-       print(#csos_order_details_data,prod_data[1][1].SKU_ITEM_ID,prod_data[2][1].NDC_ID,order_details_data[1],order_details_data[2])
-                     matched_order_details_status=0
-                            
-                                for k=1,#csos_order_details_data do
-                                    if(tostring(csos_order_details_data[k].QUANTITY)==tostring(order_details_data[k].REQ_QTY) and
-                                        tostring(csos_order_details_data[k].SIZE_OF_PACKAGE)==tostring(order_details_data[k].SHIP_UOM_DESC) and
-                                        tostring(csos_order_details_data[k].BUYER_ITEM_NUM)==tostring(prod_data[k][1].SKU_ITEM_ID) and 
-                                        tostring(csos_order_details_data[k].NATIONAL_DRUG_CDE)==tostring(prod_data[k][1].NDC_ID)
-                                        and tostring(csos_order_details_data[k].DEA_SCHEDULE)==tostring(prod_data[k][1].DEA_SCHEDULE) )
-                                    then
-                                        matched_order_details_status=matched_order_details_status+1
-                                       
-                                    end
-                                    -- order_detail_result=conn_dev:query{sql="select * from 3pl_sps_ordering.order_detail where REQ_QTY='"..csos_order_details_data[k].QUANTITY.."' and SHIP_UOM_DESC='"..csos_order_details_data[k].SIZE_OF_PACKAGE.."';",live=true};
 
-                                    -- prod_result= conn_dev:query{sql="select * from 3pl_sps_ordering.prod where SKU_ITEM_ID='"..csos_order_details_data[k].BUYER_ITEM_NUM.."' and NDC_ID='"..csos_order_details_data[k].NATIONAL_DRUG_CDE.."' and DEA_SCHEDULE='"..csos_order_details_data[k].DEA_SCHEDULE.."';",live=true};
+                            print(#csos_order_details_data,prod_data[1][1].SKU_ITEM_ID,prod_data[2][1].NDC_ID,order_details_data[1],order_details_data[2])
+                            matched_order_details_status=0
+
+                            for k=1,#csos_order_details_data do
+                                if(tostring(csos_order_details_data[k].QUANTITY)==tostring(order_details_data[k].REQ_QTY) and
+                                    tostring(csos_order_details_data[k].SIZE_OF_PACKAGE)==tostring(order_details_data[k].SHIP_UOM_DESC) and
+                                    tostring(csos_order_details_data[k].BUYER_ITEM_NUM)==tostring(prod_data[k][1].SKU_ITEM_ID) and
+                                    tostring(csos_order_details_data[k].NATIONAL_DRUG_CDE)==tostring(prod_data[k][1].NDC_ID)
+                                    and tostring(csos_order_details_data[k].DEA_SCHEDULE)==tostring(prod_data[k][1].DEA_SCHEDULE) )
+                                then
+                                    matched_order_details_status=matched_order_details_status+1
                                 end
-                          
+                            end
                             if(matched_order_details_status==#order_details_data and matched_order_details_status==#csos_order_details_data ) then
                                 soap.soaprequest()
                                 final_result=soap.getsoapresponsevalues(after_parsing)
                                 print(final_result)
                                 if(final_result=='N') then
-                                   csos_order_header_update=conn_dev:query{sql="update 3pl_sps_ordering.csos_order_header set CSOS_ORDER_HDR_STAT ='2' where CSOS_ORDER_HDR_STAT='1' ;", live=true};
+                                    csos_order_header_update=conn_dev:query{sql="update 3pl_sps_ordering.csos_order_header set CSOS_ORDER_HDR_STAT ='2' where CSOS_ORDER_HDR_STAT='1' ;", live=true};
                                     order_header_update=conn_dev:query{sql="update  3pl_sps_ordering.order_header set  ORDER_HDR_STAT_DESC='2' where CSOS_ORDER_NUM='"..csos_order_header_data[i].UNIQUE_TRANS_NUM.."' ;", live=true};
                                     if(#csos_order_header_update>0 and #order_header_update>0) then
                                         conn_dev:execute{sql=[[COMMIT;]],live=true}
@@ -97,16 +90,10 @@ function main()
                     else
                         log_file:write("Data Present in csos_order_header and order_header tables are not equal "..os.date('%x').." at :"..os.date('%X'),"\n")
                     end
-
-
                 else
-
                     log_file:write("Data Present in order_header_data or customer_billto_shipto_data is empty  "..os.date('%x').." at :"..os.date('%X'),"\n")
                     selection_status = false
-
-
                 end
-
             end
         else
             conn_dev:execute{sql=[[ROLLBACK;]],live=true}
